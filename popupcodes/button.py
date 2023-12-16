fc6912ca979ead17d1abf9cac026340b40f9d96e
rainydrterror <chan.jie.le@edgefield.edu.sg>
2023-12-16 17:54:33 +0800

    these are the popup/button/whatever code

---
+++ popupcodes/button.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import PhotoImage
+#new window
+def on_button_click():
+    new_window = tk.Toplevel(root)
+    new_window.title("New Window")
+    label = tk.Label(new_window, text="Welcome to the farm!")
+    label.pack(padx=20, pady=20)
+   
+
+
+#button
+root = tk.Tk()
+root.title("Click here!")
+button = tk.Button(root, text="Error⚠️", command=on_button_click)
+button.pack(pady=10)
+root.mainloop()
