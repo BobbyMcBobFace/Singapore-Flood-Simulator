ade401de688b652037e53c09efb9ec12b23125f4
rainydrterror <chan.jie.le@edgefield.edu.sg>
2023-12-16 22:25:27 +0800

    hello

--- popupcodes/button.py
+++ popupcodes/button.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
+
 #new window
 def on_button_click():
     new_window = tk.Toplevel(root)
-    new_window.title("New Window")
+    new_window.title("Farm")
     label = tk.Label(new_window, text="Welcome to the farm!")
     label.pack(padx=20, pady=20)
-   
 
 
 #button
