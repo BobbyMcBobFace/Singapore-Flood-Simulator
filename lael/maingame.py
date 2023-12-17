ae16f8533e967b46d6554d5f9c2583cb4ceacecd
BobbyMcBobFace <zacthebigbrother@gmail.com>
2023-12-17 08:24:46 +0800

    yay

--- lael/maingame.py
+++ lael/maingame.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import pygame
 import threading
-import subprocess  # Import subprocess module
+ import subprocess  # Import subprocess module
 
 # Initialize Pygame
 pygame.init()
