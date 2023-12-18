ed2e58f6b53889635f039aa83880962aced6d379
BobbyMcBobFace <zacthebigbrother@gmail.com>
2023-12-18 14:29:07 +0800

    made it work with image overlays

--- Game/Minigames/draganddropgame.py
+++ Game/Minigames/draganddropgame.py
@@ -26,8 +26,8 @@ for i, color in enumerate(home_colors):
     h = 30
     x = SCREEN_WIDTH // 2 - (len(home_colors) * w + (len(home_colors) - 1) * 80) // 2 + i * (w + 80)
     y = SCREEN_HEIGHT - 50  # Centered at the bottom
-    home = pygame.Rect(x, y, w, h)
-    homes.append((home, color))
+    home_rect = pygame.Rect(x, y, w, h)
+    homes.append((home_rect, color))
 
 # Create colored boxes with matching home colors
 for i in range(3):
@@ -36,38 +36,33 @@ for i in range(3):
     w = 50
     h = 50
     home_color = home_colors[i]
-    box = pygame.Rect(x, y, w, h)
-    boxes.append((box, home_color))
+    box_rect = pygame.Rect(x, y, w, h)
+    box_color = home_color  # Use home color as the box color
+    boxes.append((box_rect, box_color))
 
 # You win screen
 font = pygame.font.Font(None, 36)
 you_win_text = font.render("You Win!", True, (0, 0, 0))  # Change color to black
 you_win_rect = you_win_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
+# Overlay images for each box
+overlay_images = [pygame.image.load('photos\\appleseeds.sprite.png').convert_alpha(),
+                  pygame.image.load('photos\\orangeseeds.sprite.png').convert_alpha(),
+                  pygame.image.load('photos\\strawberryseeds.sprite.png').convert_alpha()]
+
+# Scale overlay images to match box size
+overlay_images = [pygame.transform.scale(img, (100, 100)) for img in overlay_images]
+
 run = True
 while run:
-    screen.fill("white")
-
-    # Draw homes
-    for home, color in homes:
-        pygame.draw.rect(screen, color, home)
-
-    # Draw boxes
-    for box, box_color in boxes:
-        pygame.draw.rect(screen, box_color, box)
-
-    # Check if all boxes are in their correct homes (win condition)
-    if all(home.colliderect(box) and home_color == box_color for (box, box_color), (home, home_color) in zip(boxes, homes)):
-        screen.blit(you_win_text, you_win_rect)
-        pygame.display.flip()
-        pygame.time.delay(2000)  # Display "You Win!" for 2 seconds
-        run = False
-
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                for num, (box, _) in enumerate(boxes):
-                    if box.collidepoint(event.pos):
+                for num, (box_rect, _) in enumerate(boxes):
+                    if box_rect.collidepoint(event.pos):
                         active_box = num
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -91,15 +86,32 @@ while run:
                     boxes[active_box][0].bottom = SCREEN_HEIGHT
 
                 # Check if the box is inside its home
-                for home, home_color in homes:
-                    if home.colliderect(boxes[active_box][0]):
+                for home_rect, home_color in homes:
+                    if home_rect.colliderect(boxes[active_box][0]):
                         if home_color == boxes[active_box][1]:
                             # Box is in the correct home
                             active_box = None
                             break
 
-        if event.type == pygame.QUIT:
-            run = False
+    # Draw background
+    screen.fill("white")
+
+    # Draw homes
+    for home_rect, color in homes:
+        pygame.draw.rect(screen, color, home_rect)
+
+    # Draw colored boxes
+    for box_rect, box_color in boxes:
+        pygame.draw.rect(screen, box_color, box_rect)
+
+    # Draw overlay images on top of boxes
+    for (box_rect, _), overlay_image in zip(boxes, overlay_images):
+        overlay_rect = box_rect.copy()  # Create a copy of the box rect for the overlay
+        screen.blit(overlay_image, overlay_rect.topleft)
+
+    # Check if all boxes are in their correct homes (win condition)
+    if all(home_rect.colliderect(box_rect) and home_color == box_color for (box_rect, box_color), (home_rect, home_color) in zip(boxes, homes)):
+        screen.blit(you_win_text, you_win_rect)
 
     pygame.display.flip()
 
