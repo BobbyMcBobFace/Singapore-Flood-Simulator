9d3749a0f641256fd7d2209eb40463c91772224a
Zac Cheng <zacthebigbrother@gmail.com>
2023-12-21 15:39:55 +0800

    yayay

--- Game/Minigames/Water_pump_minigame.py
+++ Game/Minigames/Water_pump_minigame.py
@@ -33,23 +33,31 @@ remaining_cracks = 0
 
 # Flag to check if the "Good Job" screen is displayed
 good_job_screen = False
+you_lose_screen = False
 
-# Function to draw the water bar
-def draw_water_bar():
-    pygame.draw.rect(screen, DARK_BLUE, (50, 50, water_bar * 5, 40))  # Changed color to dark blue
-    pygame.draw.rect(screen, RED, (50, 50, 500, 40), 2)
-    text = font.render("Water Being Lost", True, WHITE)  # Changed text inside the bar
-    text_rect = text.get_rect(center=(300, 70))  # Adjusted text position
-    screen.blit(text, text_rect)
+# Load and shrink crack image
+crack_image_original = pygame.image.load("photos\\CRACK-removebg-preview.png")  # Replace with your actual file path
+crack_image = pygame.transform.scale(crack_image_original, (int(crack_image_original.get_width() * 0.1), int(crack_image_original.get_height() * 0.1)))
+
+# Load custom cursor image and make it larger
+custom_cursor_image_original = pygame.image.load("photos\\adhesive-electrical-tape-cartoon-illustration-vector-removebg-preview.png")  # Replace with your custom cursor image path
+custom_cursor_image = pygame.transform.scale(custom_cursor_image_original, (48, 48))  # Adjust the size here
+
+# Set the custom cursor
+pygame.mouse.set_visible(False)  # Hide the default cursor
+
 
 # Function to draw the pipe at the bottom of the window
 def draw_pipe():
-    pygame.draw.rect(screen, GREY, (50, 500, 700, 40))  # Solid grey
+    pygame.draw.rect(screen, GREY, (50, 490, 700, 40))  # Solid grey
 
 # Function to draw cracks on the pipe
 def draw_cracks():
     for crack in cracks:
-        pygame.draw.circle(screen, BLACK, crack[0], 7)  # Changed color to dark blue
+        x, y = crack[0]
+        crack_value = crack[1]
+        crack_rect = crack_image.get_rect(center=(x, y))
+        screen.blit(crack_image, crack_rect.topleft)
 
 # Function to check if cracks overlap
 def overlaps(new_crack, existing_cracks):
@@ -91,11 +99,38 @@ def good_job_screen_display():
 
     return button_rect  # Return the button rectangle for click detection
 
+# Function to display "You Lose" screen
+def you_lose_screen_display():
+    screen.fill(LIGHT_BLUE)
+    text = big_font.render("You Lose!", True, DARK_BLUE)  # Adjusted text color
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+
+    # Button rectangle
+    button_rect = pygame.Rect(WIDTH // 2 - 100, HEIGHT // 2 + 50, 200, 50)
+    pygame.draw.rect(screen, RED, button_rect)
+    
+    # Button text
+    button_text = font.render("Exit", True, WHITE)
+    button_text_rect = button_text.get_rect(center=button_rect.center)
+    screen.blit(button_text, button_text_rect)
+
+    return button_rect  # Return the button rectangle for click detection
+
 # Main game loop
 running = True
 clock = pygame.time.Clock()
 
 generate_cracks()
+
+# Set the start time for the timer
+start_time = pygame.time.get_ticks()
+
+# Time limit in seconds
+time_limit = 50
+
+# Variables for win/lose screens
+good_job_screen = False
+you_lose_screen = False
 
 while running:
     for event in pygame.event.get():
@@ -106,8 +141,13 @@ while running:
                 button_rect = good_job_screen_display()
                 if button_rect.collidepoint(event.pos):
                     running = False  # Exit the game when the button is clicked
+            elif you_lose_screen:
+                button_rect = you_lose_screen_display()
+                if button_rect.collidepoint(event.pos):
+                    running = False  # Exit the game when the button is clicked
             else:
-                clicked_cracks = [crack for crack in cracks if pygame.Rect(crack[0][0] - 7, crack[0][1] - 7, 14, 14).collidepoint(event.pos)]
+                # Center the hitbox on the crack image
+                clicked_cracks = [crack for crack in cracks if pygame.Rect(crack[0][0] - crack_image.get_width() // 2, crack[0][1] - crack_image.get_height() // 2, crack_image.get_width(), crack_image.get_height()).collidepoint(event.pos)]
                 for crack in clicked_cracks:
                     water_bar -= crack[1]
                     remaining_cracks -= 1
@@ -115,21 +155,37 @@ while running:
                     if remaining_cracks == 0:
                         good_job_screen = True
 
+    # Calculate elapsed time
+    elapsed_time = (pygame.time.get_ticks() - start_time) // 1000
+    time_left = max(0, time_limit - elapsed_time)
+
+    if time_left == 0 and not good_job_screen:
+        you_lose_screen = True
+
     screen.fill(LIGHT_BLUE)
 
     draw_pipe()  # Moved the drawing of the pipe to show outline
     draw_cracks()
 
-    draw_water_bar()  # Draw water bar after other elements to appear above them
+    # Display remaining cracks
+    remaining_cracks_text = font.render(f"Cracks Left: {remaining_cracks}", True, WHITE)
+    screen.blit(remaining_cracks_text, (20, 20))
 
     if good_job_screen:
         button_rect = good_job_screen_display()
+    elif you_lose_screen:
+        button_rect = you_lose_screen_display()
+    else:
+        # Display time left
+        time_text = font.render(f"Time Left: {time_left} seconds", True, WHITE)
+        screen.blit(time_text, (WIDTH - time_text.get_width() - 20, 20))
+
+    # Update the custom cursor position
+    screen.blit(custom_cursor_image, pygame.mouse.get_pos())
 
     pygame.display.flip()
 
     clock.tick(60)
-
-
 
 pygame.quit()
 sys.exit()
